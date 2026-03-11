3a87e621b4ad996d53d41a1ec8b7999e4fa49b0a
Manash Krit <manashkrit06@gmail.com>
2026-03-11 10:01:41 +0530

    multer setup

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,26 +1,24 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message="Something went wrong",
-        error=[],
-        statck=""
-    ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success=false;
-        // detected success flag would get false
-        this.errors=errors
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    // detected success flag would get false
+    this.errors = errors;
 
-        if(statck){
-            this=stack=statck
-        } else{
-            Error.captureStackTrace(this,this.
-                constructor)
-        }
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-
-export {ApiError}
+export { ApiError };
